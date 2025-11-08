e584923c3cf538eaf7184d47901b2f6c6bc62a1b
xswzaq123 <qazwsxxswzaq0192837465@gmail.com>
2025-11-08 16:29:13 +0530

    update state machine

--- state_machine.gd
+++ state_machine.gd
@@ -4,24 +4,24 @@ extends WeakRef
 signal state_changed(prev_state: int)
 
 var states: Dictionary[int, aBaseStateInterface]
-var current_state: aBaseStateInterface
+var _current_state: aBaseStateInterface
 var _current_state_key: int
-var owner: WeakRef
+var _owner: WeakRef
 
 
 func update(delta: float) -> void:
-	if current_state:
-		current_state.update(delta)
+	if _current_state:
+		_current_state.update(delta)
 
 
 func physics_update(delta: float) -> void:
-	if current_state:
-		current_state.physics_update(delta)
+	if _current_state:
+		_current_state.physics_update(delta)
 
 
 func handle_input(event: InputEvent) -> void:
-	if current_state:
-		current_state.handle_input(event)
+	if _current_state:
+		_current_state.handle_input(event)
 
 
 func add_state(state_key: int, state: aStateInterface) -> void:
@@ -30,16 +30,16 @@ func add_state(state_key: int, state: aStateInterface) -> void:
 
 
 func change_state(new_state_key: int) -> void:
-	var prev_state_key: int = _current_state_key
-	if current_state:
-		current_state.exit()
+	var prev_state_key: int =  _current_state_key
+	if _current_state:
+		_current_state.exit()
 
 	_current_state_key = new_state_key
-	current_state = states.get(_current_state_key)
+	_current_state = states.get( _current_state_key)
 
 
-	if current_state:
-		current_state.enter(prev_state_key)
+	if _current_state:
+		_current_state.enter(prev_state_key)
 		state_changed.emit(prev_state_key)
 	else:
 		push_warning(get_current_state_name() + " state not found")
@@ -49,17 +49,30 @@ func set_initial_state(state_key: int) -> void:
 	change_state(state_key)
 
 
-func get_current_state() -> int:
+## Returns reference to the state
+func get_current_state() -> aBaseStateInterface:
+	if not _current_state:
+		return null
+	return _current_state
+
+
+## Returns the key of the state
+func get_current_state_key() -> int:
 	return _current_state_key
 
 
-func get_current_state_name() -> String:
-	return get_owner().States.keys()[_current_state_key]
+## Returns the name of the state
+func get_current_state_name() -> StringName:
+	var state_name: String = get_owner().States.keys()[_current_state_key]
+	if state_name:
+		return state_name
+	else:
+		return ""
 
 
-func set_owner(_owner: Variant) -> void:
-	owner = weakref(_owner)
+func set_owner(node: Variant) -> void:
+	_owner = weakref(node)
 
 
 func get_owner() -> Variant:
-	return owner.get_ref()
+	return _owner.get_ref()
